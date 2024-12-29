b61eb64d1d0c96c733624b12f1dbc5ce231ac80e
Vanilla <vanilla@nya.gg>
2024-12-29 15:24:21 +0100

    Replace Thymeleaf with JSF - This reminds me why ihatejsf.com exists.

--- src/main/java/gg/nya/imagehosting/ImagehostingApplication.java
+++ src/main/java/gg/nya/imagehosting/ImagehostingApplication.java
@@ -2,8 +2,10 @@ package gg.nya.imagehosting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class ImagehostingApplication {
 
 	public static void main(String[] args) {
