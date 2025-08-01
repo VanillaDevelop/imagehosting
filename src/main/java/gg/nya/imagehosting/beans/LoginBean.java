7e84e9652b995b7525b42f8a8bc4d2b9311b593c
Vanilla <vanilla@nya.gg>
2025-08-01 22:10:03 +0200

    Login Bean view scoped

--- src/main/java/gg/nya/imagehosting/beans/LoginBean.java
+++ src/main/java/gg/nya/imagehosting/beans/LoginBean.java
@@ -1,24 +1,34 @@
 package gg.nya.imagehosting.beans;
 
+import gg.nya.imagehosting.config.ApplicationContextProvider;
 import gg.nya.imagehosting.models.User;
 import gg.nya.imagehosting.services.AuthenticationService;
 import jakarta.faces.application.FacesMessage;
 import jakarta.faces.context.FacesContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.Optional;
 
 @Component("loginBean")
-public class LoginBean {
+@Scope("view")
+public class LoginBean implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     private String username;
     private String password;
 
     private static final Logger log = LoggerFactory.getLogger(LoginBean.class);
 
-    private final AuthenticationService authenticationService;
+    private transient AuthenticationService authenticationService;
 
     public LoginBean(AuthenticationService authenticationService) {
         this.authenticationService = authenticationService;
@@ -103,4 +113,12 @@ public class LoginBean {
 
         return null;
     }
+
+     @Serial
+     private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+         in.defaultReadObject();
+         // Retrieve the Spring application context (using a helper or static context holder)
+         ApplicationContext ctx = ApplicationContextProvider.getApplicationContext();
+         this.authenticationService = ctx.getBean(AuthenticationService.class);
+     }
 }
